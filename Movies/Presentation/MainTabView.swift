cf831722127eb32efb240db3145890bc656aa0dd
Mahmoud Elshakoushy <melshako@kenvue.com>
2024-05-27 14:30:35 +0300

    Organize project structure

---
+++ Movies/Presentation/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  Movies
+//
+//  Created by Mahmoud Elshakoushy on 27/05/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            EmptyView()
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
