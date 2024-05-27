d6263e6e9bbeca0e5b176cb00bd33b25620c7a7d
Mahmoud Elshakoushy <melshako@kenvue.com>
2024-05-27 14:08:41 +0300

    Create project

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Mahmoud Elshakoushy on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
